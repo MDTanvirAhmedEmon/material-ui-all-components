7148d4ce698beb441c8cb6ec52c155c647f8b32a
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2024-10-12 18:14:50 +0600

    explore button

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Button variant="contained">Contained</Button>
+      <Button
+        sx={{
+          bgcolor: "red",
+          px: 6
+        }}
+      variant="contained">Contained</Button>
     </div>
   );
 }
